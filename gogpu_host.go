d80fa60ecf9eb32ca4e99c0cf8e1b56f2aacc70c
Ivan Sorokin <unxed@mail.ru>
2026-08-03 20:22:26 +0200

    Disable the `gogpu` backend in `vtui` on 32-bit ARM (arm) to avoid compiling `goffi`.

--- gogpu_host.go
+++ gogpu_host.go
@@ -1,4 +1,4 @@
-//go:build !freebsd && !dragonfly && !openbsd && !netbsd && !illumos && !solaris
+//go:build !freebsd && !dragonfly && !openbsd && !netbsd && !illumos && !solaris && !arm
 
 package vtui
 
--- gogpu_renderer.go
+++ gogpu_renderer.go
@@ -1,4 +1,5 @@
-//go:build !freebsd && !dragonfly && !openbsd && !netbsd && !illumos && !solaris
+//go:build !freebsd && !dragonfly && !openbsd && !netbsd && !illumos && !solaris && !arm
+
 package vtui
 
 import (
