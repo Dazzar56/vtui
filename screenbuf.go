7b2520b45f7e5a55d910c4b0b860bb52ddecee65
Ivan Sorokin <unxed@mail.ru>
2026-08-12 15:58:16 +0200

    screenRow reads a stretch of one row back out of the screen.

--- screenbuf.go
+++ screenbuf.go
@@ -742,3 +742,16 @@ func (s *ScreenBuf) GetCursorPos() (int, int) {
 	defer s.mu.Unlock()
 	return s.cursorX, s.cursorY
 }
+
+// screenRow reads a stretch of one row back out of the screen.
+func screenRow(scr *vtui.ScreenBuf, y, x1, x2 int) string {
+	runes := make([]rune, x2-x1+1)
+	for i := range runes {
+		cell := scr.GetCell(x1+i, y)
+		runes[i] = rune(cell.Char)
+		if runes[i] == 0 {
+			runes[i] = ' '
+		}
+	}
+	return string(runes)
+}
